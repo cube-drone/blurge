c411ae66bc17e4b47617064f356c57abb74b1f6a
Curtis Lassam <curtis@lassam.net>
2012-01-01 12:29:00 -0800

    Tidying the core game code.

--- flup.py
+++ flup.py
@@ -1,51 +1,104 @@
+# External Libraries
+from pymongo import Collection
 
-from state_grid import StateGrid as TokenGrid
+# Local Libraries
+from state_grid import StateGrid as Grid
 import tokens
+
+# Built-in Libraries
 import random
 import copy
 
 class Game( object ):
     
-    def generate( self, mongo_id ):
-        self.grid = TokenGrid( width, height )
-        pass
-
-    def generate( self, width, height, gametype, ntokens):
-        self.grid = TokenGrid( width, height )
+    def generate( self, width=10, height=10, gametype="Default", ntokens=8,
+                    nturns=10):
+        """ Generate a new game and save it to the database. 
+            width/height - width & height of the generated grid. 
+            gametype - "Clear", "Solution", or "Default" 
+            ntokens - the number of different token types to use
+            nturns - the number of turns to pre-play in a Default game, OR
+                     the numver of turns to rewind in a Solution game
+        """
+        self.grid = Grid( width, height )
         self.tokens = tokens.selectRandomNTokens( ntokens )  
-        self.laziness = 30
-        self.numberOfInitialTokens = 100
+        self.nturns = nturns
+        self.laziness = 30 #If we've already found 30 solutions, stop looking. 
+        self.gamestate = "Playable" # "Playable" || "Unplayable" 
             
         # Initialize Board
         if gametype == "Clear":
-            pass
+            self.setup_clear_game()
         elif gametype == "Solution":
-            self.completelySolve()
-            for i in range(0, 10):
-                self.grid.rewindLastFullMove()  
-        else: #default gametype
-            self.ten_turns_in()
+            self.setup_solution_game()
+        else:
+            self.setup_default_game()
 
         self.selectValidToken()
+        self.save()
+    
+    def load( self, mongo_id ):
+        self.grid = Grid( width, height )
+        pass
     
     def save( self, mongo_id = 0):
         pass
     
-    def ten_turns_in( self ):
-        for i in range( 0, 10 ):
+    def setup_default_game( self ):
+        """ In a Default game, we play n turns for the player. """
+        for i in range( 0, self.nturns ):
             self.autoplayOneTurn()
-
+    
+    def setup_clear_game( self ):
+        """ In a Clear game, we play 0 turns for the player. """
+        pass   
+ 
+    def setup_solution_game( self ):
+        """ In a Solution game, we solve the game, then rewind n turns. """
+        self.completelySolve()
+        for i in range(0, self.nturns):
+            self.grid.rewindLastFullMove()  
+    
+    
     def selectValidToken( self ):
+        """ Set a verifiably playable token to the current token. """
         result = self.solveOneStep()
         if not result: 
-            self.gameOver = True
+            self.gameState = "Unplayable" 
             return 
         token, point = result
         self.currentToken = token
     
     def completelySolve( self ):
-        while  self.autoplayOneTurn( ) :
+        """ Completely solve the game. (Currently brute-force and non-optimal) """
+        while self.autoplayOneTurn( ) :
             pass
+    
+    def autoplayOneTurn( self ): 
+        """ Completely play one turn for the player. """
+        token_point = self.solveOneStep()
+        if not token_point:
+            return False
+        token, point = token_point
+        if self.grid.placeToken( token, point ):
+            return True
+        else:
+            print "Somehow our valid placement is not valid - ", point
+            print self.grid.error
+            return self.autoplayOneTurn( )
+
+    def solveOneStep( self, temp_tokens = [] ):
+        """ Return the solution to one step of the game. """ 
+        if temp_tokens == []:
+            temp_tokens = copy.deepcopy( self.tokens ) 
+        test_token = random.choice( temp_tokens )
+        
+        valid_placements = self.solveForToken( test_token ) 
+        if len(valid_placements) == 0 and len(temp_tokens) <= 1:
+            return False
+        if len(valid_placements) == 0:
+            temp_tokens.remove( test_token )
+            return self.solveOneStep( temp_tokens )
     
     def solveForToken( self, token ):
         """ Returns all valid placements for the token. """ 
@@ -57,34 +110,10 @@ class Game( object ):
                 break
         return valid_placements
 
-    def solveOneStep( self, temp_tokens = [] ):
-        if temp_tokens == []:
-            temp_tokens = copy.deepcopy( self.tokens ) 
-        test_token = random.choice( temp_tokens )
-        
-        valid_placements = self.solveForToken( test_token ) 
-        if len(valid_placements) == 0 and len(temp_tokens) <= 1:
-            return False
-        if len(valid_placements) == 0:
-            temp_tokens.remove( test_token )
-            return self.solveOneStep( temp_tokens )
-
         place_point = random.choice(valid_placements)
         print test_token.name(), place_point
         return ( test_token, place_point ) 
 
-    def autoplayOneTurn( self ): 
-        token_point = self.solveOneStep()
-        if not token_point:
-            return False
-        token, point = token_point
-        if self.grid.placeToken( token, point ):
-            return True
-        else:
-            print "Somehow our valid placement is not valid - ", point
-            print self.grid.error
-            return self.autoplayOneTurn( )
- 
     def get_last_delta( self ):
         pass
     
